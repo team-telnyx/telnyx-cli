04c8306c70d8638703e1725a21a1673568264e36
Gerson Scanapieco <gerson@telnyx.com>
2022-07-22 18:20:01 -0300

    Add initialization command

---
+++ cmd/init.go
@@ -0,0 +1,60 @@
+/*
+Copyright © 2022 Telnyx LLC
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/team-telnyx/telnyx-cli/config"
+	"gopkg.in/yaml.v3"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initializes the configuration file",
+	Run: func(cmd *cobra.Command, args []string) {
+		res, err := config.ConfigExists()
+		if res {
+			fmt.Println("Already configured!")
+			os.Exit(0)
+		} else if err != nil {
+			cobra.CheckErr(err)
+		}
+
+		defaultConfig, err := yaml.Marshal(config.DefaultConfig())
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+
+		if err := os.Mkdir(config.DefaultConfigFolder(), os.ModePerm); err != nil {
+			cobra.CheckErr(err)
+		}
+
+		fmt.Printf("Creating configuration file in %s\n", config.DefaultConfigPath())
+
+		f, err := os.Create(config.DefaultConfigPath())
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+
+		defer f.Close()
+
+		_, err = f.Write(defaultConfig)
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+
+		f.Sync()
+
+		fmt.Println("Done")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+}
---
+++ config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"errors"
+	"os"
+	"path"
+
+	"github.com/spf13/cobra"
+)
+
+const ConfigFolder = ".telnyx-cli"
+const ConfigFile = "config.yaml"
+
+type Config struct {
+	ConsulDevUrl  string `yaml:"consul_dev_url"`
+	ConsulProdUrl string `yaml:"consul_prod_url"`
+}
+
+func DefaultConfig() *Config {
+	return &Config{
+		ConsulDevUrl:  "https://consuldev.internal.telnyx.com",
+		ConsulProdUrl: "https://consul.internal.telnyx.com",
+	}
+}
+
+func DefaultConfigFolder() string {
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+
+	return path.Join(home, ConfigFolder)
+}
+
+func DefaultConfigPath() string {
+	return path.Join(DefaultConfigFolder(), ConfigFile)
+}
+
+func ConfigExists() (bool, error) {
+	_, err := os.Stat(DefaultConfigPath())
+	if err == nil {
+		return true, nil
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
+}
